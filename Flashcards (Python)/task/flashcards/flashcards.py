754c79dc75ac12fa928bd6f4ff376aa3231307f1
AleksandrAfanasev <jgBeM9bkX@@hEq3>
2023-03-18 17:20:47 +0300

    Stage 3/7: Make it your own  User decide how many cards they would like to make. First, ask the player to enter the desired number of cards. Then, ask them to input the term and the definition for every flashcard.

--- Flashcards (Python)/task/flashcards/flashcards.py
+++ Flashcards (Python)/task/flashcards/flashcards.py
@@ -1,10 +1,25 @@
-term = input()
-definition = input()
-answer = input()
+def make_cards():
+    cards = {}
+    number_of_flashcards = int(input('Input the number of cards:\n'))
+    for i in range(1, number_of_flashcards + 1):
+        term = input(f'The term for card #{i}:\n')
+        definition = input(f'The definition for card #{i}:\n')
+        cards[definition] = term
+    return cards
 
-cards = {}
-cards[term] = definition
-if answer == cards[term]:
-    print('Your answer is right!')
-else:
-    print('Your answer is wrong...')
+
+def check_answer(cards):
+    for card in cards:
+        answer = input(f'Print the definition of "{cards[card]}":\n')
+        if answer == card:
+            print('Correct!')
+        else:
+            print(f'Wrong. The right answer is "{card}".')
+
+
+def main():
+    check_answer(make_cards())
+
+
+if __name__ == '__main__':
+    main()
